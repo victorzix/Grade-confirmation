df78fd051c1afef39043374a94bca721314b642e
victorzix <linkgamesplays@gmail.com>
2022-12-01 13:11:50 -0300

    Grade Thing

---
+++ scripts.js
@@ -0,0 +1,19 @@
+function subMit() {
+  var value = document.getElementById("grade").value;
+  let result = "";
+  let color;
+    
+    if (value < 60) {
+    result = "F";   color = 'red'
+  } else if (value >= 60 && value <= 69) {
+    result = "D";  color = 'red'
+  } else if (value >= 70 && value <= 79) {
+    result = "C";  color = 'yellow'
+  } else if (value >= 80 && value <= 89) {
+    result = "B"; color = 'green'
+  } else if (value >= 90) {
+    result = "A";  color = 'green'
+  }
+document.getElementById("result").textContent = "This is your grade:  " + result;
+document.getElementById('form').style.backgroundColor = color
+}
